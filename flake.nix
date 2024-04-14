fc2ca679f7ec37c78e2931aa3df88f7419936334
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2024-04-14 15:55:21 +0530

    Add nix flakes

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs"; };
+
+  outputs = { self, nixpkgs }:
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      devShell.x86_64-linux =
+        pkgs.mkShell { 
+          name = "python-devel";
+          venvDir = "venv";
+          buildInputs = with pkgs.python311Packages; [
+            pandas
+            numpy
+            scipy
+            jupyter
+            yfinance
+            icecream
+            venvShellHook
+            pyarrow
+          ]; 
+        };
+   };
+}
